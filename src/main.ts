0be8105b8eb4d3e196dbd10e24316923fd5606af
Vasile Cojusco <vasilecojusco@192.168.1.111>
2023-12-05 21:57:33 +0000

    init

---
+++ src/main.ts
@@ -0,0 +1,60 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer } from 'class-validator';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  const baseUrl =
+    configService
+      .get<string>('APP_URL', 'https://localhost:3000')
+      ?.split(',') || [];
+  const baseMode = configService.get<string>('BASE_MODE');
+
+  app.enableCors({
+    origin:
+      baseMode === 'dev' || baseMode === 'stg'
+        ? [
+            `http://localhost:3000`,
+            `http://127.0.0.1:3000`,
+            `http://0.0.0.0:3000`,
+            ...baseUrl,
+          ]
+        : baseUrl,
+    credentials: true,
+    exposedHeaders: ['Content-Disposition'],
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+
+  const config = new DocumentBuilder()
+    .addApiKey({ type: 'apiKey', name: 'X-API-KEY', in: 'header' }, 'X-API-KEY')
+    .addBearerAuth()
+    .setTitle('Legitify API')
+    .setDescription(
+      'You can find documentation for all endpoints on Legitify website.',
+    )
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const host = configService.get<string>('HOST', 'localhost');
+  const port = configService.get<number>('PORT', 3333);
+
+  await app.listen(port, host, () => {
+    console.info(`API server is running on http://${host}:${port}`);
+  });
+}
+bootstrap();
